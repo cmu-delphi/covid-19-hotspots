5a9d9e8ac392a2ef3592c29a6222674e26bde6ad
David Zhao <dzhaoism@gmail.com>
2020-09-15 18:52:41 -0400

    a few more tests

--- test_hotspots.R
+++ test_hotspots.R
@@ -1,7 +1,7 @@
 setwd("C:/Users/dzhao/Documents/Carnegie Mellon 2018-2023/RESEARCH - Delphi/covid-19-hotspots")
 load("testdata_mat.RData")
 load("testdata_df_model.RData")
-source("helpers_dz.r")
+source("helpers.r")
 library(testthat)
 
 lags <- 28
@@ -25,7 +25,12 @@ test_that("check raw df", {
 
 
 test_that("check ready to model df", {
-  
+  expect_true(as.logical(prod(c("geo_value", "time_value", "resp") %in% names(df_model))))
+  expect_equal(n_distinct(df_model$geo_value), 52)
+  expect_equal(min(df_model$time_value), start_day + 5)
+  expect_equal(any(is.na(df_model)), FALSE)
+  expect_true(max(df_model$resp) == 1)
+  expect_true(min(df_model$resp) == 0)
 })
 
 
@@ -45,6 +50,14 @@ test_that("check ROC plots", {
 
 
 test_that("check AUC plots", {
+  
+  # find one example of a model, compute AUC and check it by eye
+  # can use pROC:auc
+  
+  # find one model, test its ROC separately (with code I wrote, see that answers match)
+  
+  # can further modularize it
+  # 
   
 })
 
